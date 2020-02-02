297f369ed5db58bc9130580046d0929ea88b8980
Ilie Cristian Dorobat <idorobat@cisco.com>
2020-02-02 22:39:22 +0200

    Edit the README.md & add the output directories

--- src/ro/webdata/translator/edm/approach/object/dspace/Main.java
+++ src/ro/webdata/translator/edm/approach/object/dspace/Main.java
@@ -1,6 +1,7 @@
 package ro.webdata.translator.edm.approach.object.dspace;
 
 import org.apache.jena.rdf.model.Model;
+import ro.webdata.common.utils.FileUtils;
 import ro.webdata.common.utils.ModelUtils;
 import ro.webdata.translator.edm.approach.object.dspace.common.constants.FileConstants;
 
@@ -10,13 +11,13 @@ public class Main {
     public static void main(String[] args) {
         Model model = ModelUtils.generateModel();
         DSpaceMapping.processing(model, FileConstants.PATH_INPUT_DSPACE_DIR);
-        writeRDFGraph(model, null);
+        writeRDFGraph(model, FileConstants.PATH_OUTPUT_DATASET_FILE, false);
     }
 
-    private static void writeRDFGraph(Model model, String outputFilePath) {
+    private static void writeRDFGraph(Model model, String outputFilePath, boolean append) {
         StringWriter writer = new StringWriter();
         model.write(writer, ModelUtils.SYNTAX_RDF_XML);
-//        FileUtils.write(writer, outputFilePath);
+        FileUtils.write(writer, outputFilePath, append);
         String result = writer.toString();
         System.out.println(result);
     }
--- src/ro/webdata/translator/edm/approach/object/dspace/common/constants/FileConstants.java
+++ src/ro/webdata/translator/edm/approach/object/dspace/common/constants/FileConstants.java
@@ -3,4 +3,7 @@ package ro.webdata.translator.edm.approach.object.dspace.common.constants;
 public class FileConstants extends ro.webdata.common.constants.FileConstants {
     public static final String PATH_INPUT_DSPACE_DIR = PATH_INPUT_DIR + FILE_SEPARATOR + "dspace";
     public static final String PATH_OUTPUT_DSPACE_DIR = PATH_OUTPUT_DIR + FILE_SEPARATOR + "dspace2edm";
+    public static final String PATH_OUTPUT_DATASET_FILE = PATH_OUTPUT_DSPACE_DIR
+            + FILE_SEPARATOR + "dataset"
+            + FILE_EXTENSION_SEPARATOR + FILE_EXTENSION_RDF;
 }
